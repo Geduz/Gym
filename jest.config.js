30aeb4a5238f09d2bfbc79a918808d7c2cd11bbe
Geduz <159464081+Geduz@users.noreply.github.com>
2025-07-08 23:20:48 -0500

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transformIgnorePatterns: ['/node_modules/'],
+  verbose: true,
+};
